b5ddb25cb07874401c8c2f496b5826e9043a6a78
ChinhHV-111 <hoangvanchinh1112005@gmail.com>
2025-11-19 21:43:40 +0700

    Chính add

--- src/main/java/org/example/btlcsdl/Main.java
+++ src/main/java/org/example/btlcsdl/Main.java
@@ -7,13 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class ClinicManagementApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ClinicManagementApplication.class.getResource("templates/clinic-management-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("templates/clinic-management-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Clinic Management System");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
